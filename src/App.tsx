1b6008e6072f83e6e64484184f16ac87e11642f6
Alinochka07 <alina011988@icloud.com>
2024-01-05 11:43:51 +0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 import { Main } from "./components/main/Main";
 
@@ -8,6 +9,7 @@ function App () {
     <>
       <Header/>
       <Main />
+      <Footer/>
     </>
   );
 }
